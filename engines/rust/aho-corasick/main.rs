65945abd27648bc924437834c36888d286d6d7c1
Andrew Gallant <jamslam@gmail.com>
2023-04-28 14:11:35 -0400

    engines/rust/aho-corasick: add 'teddy' engine
    I've been trying to diagnose some performance bugs with Teddy. Or more
    generally, how Teddy is called. As part of that debugging, it was useful
    to be able to run Teddy searches specifically in the rebar harness.

--- engines/rust/aho-corasick/main.rs
+++ engines/rust/aho-corasick/main.rs
@@ -1,7 +1,7 @@
 use std::io::Write;
 
 use {
-    aho_corasick::{AhoCorasick, AhoCorasickKind, MatchKind},
+    aho_corasick::{packed, AhoCorasick, AhoCorasickKind, MatchKind},
     anyhow::Context,
     bstr::ByteSlice,
     lexopt::{Arg, ValueExt},
@@ -17,11 +17,6 @@ fn main() -> anyhow::Result<()> {
                 anyhow::Error::from(arg.unexpected()).context("<engine>")
             );
         }
-    };
-    let kind = match &*engine {
-        "dfa" => AhoCorasickKind::DFA,
-        "nfa" => AhoCorasickKind::ContiguousNFA,
-        _ => anyhow::bail!("unrecognized engine '{}'", engine),
     };
     let (mut quiet, mut version) = (false, false);
     while let Some(arg) = p.next()? {
@@ -44,11 +39,17 @@ fn main() -> anyhow::Result<()> {
     }
     let b = klv::Benchmark::read(std::io::stdin())
         .context("failed to read KLV data from <stdin>")?;
+    let kind = match &*engine {
+        "dfa" => AhoCorasickKind::DFA,
+        "nfa" => AhoCorasickKind::ContiguousNFA,
+        "teddy" => return main_teddy(&b, quiet),
+        _ => anyhow::bail!("unrecognized engine '{}'", engine),
+    };
     let samples = match b.model.as_str() {
-        "compile" => model_compile(&b, kind)?,
-        "count" => model_count(&b, &compile(&b, kind)?)?,
-        "count-spans" => model_count_spans(&b, &compile(&b, kind)?)?,
-        "grep" => model_grep(&b, &compile(&b, kind)?)?,
+        "compile" => model_compile_ac(&b, kind)?,
+        "count" => model_count_ac(&b, &compile_ac(&b, kind)?)?,
+        "count-spans" => model_count_spans_ac(&b, &compile_ac(&b, kind)?)?,
+        "grep" => model_grep_ac(&b, &compile_ac(&b, kind)?)?,
         _ => anyhow::bail!("unsupported benchmark model '{}'", b.model),
     };
     if !quiet {
@@ -60,7 +61,24 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn model_compile(
+fn main_teddy(b: &klv::Benchmark, quiet: bool) -> anyhow::Result<()> {
+    let samples = match b.model.as_str() {
+        "compile" => model_compile_teddy(&b)?,
+        "count" => model_count_teddy(&b, &compile_teddy(&b)?)?,
+        "count-spans" => model_count_spans_teddy(&b, &compile_teddy(&b)?)?,
+        "grep" => model_grep_teddy(&b, &compile_teddy(&b)?)?,
+        _ => anyhow::bail!("unsupported benchmark model '{}'", b.model),
+    };
+    if !quiet {
+        let mut stdout = std::io::stdout().lock();
+        for s in samples.iter() {
+            writeln!(stdout, "{},{}", s.duration.as_nanos(), s.count)?;
+        }
+    }
+    Ok(())
+}
+
+fn model_compile_ac(
     b: &klv::Benchmark,
     kind: AhoCorasickKind,
 ) -> anyhow::Result<Vec<timer::Sample>> {
@@ -68,11 +86,11 @@ fn model_compile(
     timer::run_and_count(
         b,
         |re: AhoCorasick| Ok(re.find_iter(haystack).count()),
-        || compile(b, kind),
+        || compile_ac(b, kind),
     )
 }
 
-fn model_count(
+fn model_count_ac(
     b: &klv::Benchmark,
     re: &AhoCorasick,
 ) -> anyhow::Result<Vec<timer::Sample>> {
@@ -80,15 +98,15 @@ fn model_count(
     timer::run(b, || Ok(re.find_iter(haystack).count()))
 }
 
-fn model_count_spans(
+fn model_count_spans_ac(
     b: &klv::Benchmark,
     re: &AhoCorasick,
 ) -> anyhow::Result<Vec<timer::Sample>> {
     let haystack = &*b.haystack;
-    timer::run(b, || Ok(re.find_iter(haystack).map(|m| m.span().len()).sum()))
+    timer::run(b, || Ok(re.find_iter(haystack).map(|m| m.len()).sum()))
 }
 
-fn model_grep(
+fn model_grep_ac(
     b: &klv::Benchmark,
     re: &AhoCorasick,
 ) -> anyhow::Result<Vec<timer::Sample>> {
@@ -104,13 +122,13 @@ fn model_grep(
     })
 }
 
-fn compile(
+fn compile_ac(
     b: &klv::Benchmark,
     kind: AhoCorasickKind,
 ) -> anyhow::Result<AhoCorasick> {
     anyhow::ensure!(
         !(b.regex.unicode && b.regex.case_insensitive),
-        "rust/aho-corasick engine is incompatible with 'unicode = true' and \
+        "rust/aho-corasick engines are incompatible with 'unicode = true' and \
          'case-insensitive = true'"
     );
     let ac = AhoCorasick::builder()
@@ -120,4 +138,62 @@ fn compile(
         .prefilter(false)
         .build(&b.regex.patterns)?;
     Ok(ac)
+}
+
+fn model_compile_teddy(
+    b: &klv::Benchmark,
+) -> anyhow::Result<Vec<timer::Sample>> {
+    let haystack = &*b.haystack;
+    timer::run_and_count(
+        b,
+        |re: packed::Searcher| Ok(re.find_iter(haystack).count()),
+        || compile_teddy(b),
+    )
+}
+
+fn model_count_teddy(
+    b: &klv::Benchmark,
+    re: &packed::Searcher,
+) -> anyhow::Result<Vec<timer::Sample>> {
+    let haystack = &*b.haystack;
+    timer::run(b, || Ok(re.find_iter(haystack).count()))
+}
+
+fn model_count_spans_teddy(
+    b: &klv::Benchmark,
+    re: &packed::Searcher,
+) -> anyhow::Result<Vec<timer::Sample>> {
+    let haystack = &*b.haystack;
+    timer::run(b, || Ok(re.find_iter(haystack).map(|m| m.len()).sum()))
+}
+
+fn model_grep_teddy(
+    b: &klv::Benchmark,
+    re: &packed::Searcher,
+) -> anyhow::Result<Vec<timer::Sample>> {
+    let haystack = &*b.haystack;
+    timer::run(b, || {
+        let mut count = 0;
+        for line in haystack.lines() {
+            if re.find(line).is_some() {
+                count += 1;
+            }
+        }
+        Ok(count)
+    })
+}
+
+fn compile_teddy(b: &klv::Benchmark) -> anyhow::Result<packed::Searcher> {
+    anyhow::ensure!(
+        !b.regex.case_insensitive,
+        "rust/aho-corasick/teddy engine is incompatible with \
+         'case-insensitive = true'"
+    );
+    let searcher = packed::Config::new()
+        .match_kind(packed::MatchKind::LeftmostFirst)
+        .builder()
+        .extend(&b.regex.patterns)
+        .build()
+        .ok_or_else(|| anyhow::anyhow!("failed to build Teddy searcher"))?;
+    Ok(searcher)
 }
